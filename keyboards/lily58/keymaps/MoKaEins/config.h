f63e492b2ed0d437d82ee7ebb54dc771f663b96c
MoKaEins <150668160+MoKaEins@users.noreply.github.com>
2023-12-02 21:47:25 +0100

    tapdance test

--- keyboards/lily58/keymaps/MoKaEins/config.h
+++ keyboards/lily58/keymaps/MoKaEins/config.h
@@ -30,8 +30,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define SPLIT_USB_TIMEOUT 2000
 #define SPLIT_USB_TIMEOUT_POLL 10
 
-#define QUICK_TAP_TERM 0
-#define TAPPING_TERM 100
+// #define QUICK_TAP_TERM 0
+#define TAPPING_TERM 175
 
 #undef RGBLED_NUM
 /*#define RGBLIGHT_EFFECT_BREATHING
--- keyboards/lily58/keymaps/MoKaEins/keymap.c
+++ keyboards/lily58/keymaps/MoKaEins/keymap.c
@@ -8,16 +8,26 @@ enum layer_number {
   _UMLAUT,
 };
 
-//uses configs made in QMK configurator
+
+// Tap Dance declarations
+/*enum {
+    TD_A_AE,
+};
+
+// Tap Dance definitions
+tap_dance_action_t tap_dance_actions[] = {
+    // Tap once for A, twice for umlautA
+    [TD_A_AE] = ACTION_TAP_DANCE_DOUBLE(KC_A, KC_QUOT),
+};*/
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
- [_BASE] =  LAYOUT(
-      KC_ESC, KC_1, KC_2, KC_3, KC_4, KC_5,                                 KC_6, KC_7, KC_8, KC_9, KC_0, KC_MINS,
-      KC_TAB, KC_Q, KC_W, KC_F, KC_P, KC_G,                                 KC_J, KC_L, KC_U, KC_Y, KC_RBRC, KC_BSLS,
-      KC_LSFT, KC_A, KC_R, KC_S, KC_T, KC_D,                                KC_H, KC_N, KC_E, KC_I, KC_O, KC_RSFT,
-      MO(_UMLAUT), KC_Z, KC_X, KC_C, KC_V, KC_B, MO(_NUM),      MO(_FKEYS), KC_K, KC_M, KC_COMM, KC_DOT, KC_SLSH, LT(_UMLAUT, KC_RCTL),
-      KC_LGUI,      KC_LALT,      KC_LCTL,      KC_SPC,      KC_SPC,      KC_BSPC,      KC_ENT,      MO(_ARROW)
+[_BASE] =  LAYOUT(
+      KC_ESC, KC_1, KC_2, KC_3, KC_4, KC_5,                                              KC_6, KC_7, KC_8, KC_9, KC_0, KC_MINS,
+      KC_TAB, KC_Q, KC_W, KC_F, KC_P, KC_G,                                              KC_J, KC_L, KC_U, KC_Y, KC_RBRC, KC_BSLS,
+      KC_LSFT, KC_A, KC_R, KC_S, KC_T, KC_D,                                             KC_H, KC_N, KC_E, KC_I, KC_O, KC_RSFT,
+      MO(_UMLAUT), KC_Z, KC_X, KC_C, KC_V, KC_B, MO(_NUM),                               MO(_FKEYS), KC_K, KC_M, KC_COMM, KC_DOT, KC_SLSH, LT(_UMLAUT, KC_RCTL),
+      KC_LGUI, KC_LALT, KC_LCTL, KC_SPC,                                 KC_SPC, KC_BSPC, KC_ENT, MO(_ARROW)
 ),
 [_FKEYS] = LAYOUT(
       KC_GRV, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5,                     KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11,
