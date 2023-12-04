f9aec3ca3ddef9c8157f350768185bd2cce46ab0
MoKaEins <150668160+MoKaEins@users.noreply.github.com>
2023-12-04 14:55:41 +0100

    tapdance keymap

--- keyboards/lily58/keymaps/MoKaEins/rules.mk
+++ keyboards/lily58/keymaps/MoKaEins/rules.mk
@@ -9,4 +9,4 @@ AUDIO_ENABLE = no           # Audio output
 RGBLIGHT_ENABLE = no       # Enable WS2812 RGB underlight.
 SWAP_HANDS_ENABLE = no      # Enable one-hand typing
 OLED_ENABLE= no             # OLED display
-TAP_DANCE_ENABLE = no      # OLED display
+TAP_DANCE_ENABLE = yes      # OLED display
