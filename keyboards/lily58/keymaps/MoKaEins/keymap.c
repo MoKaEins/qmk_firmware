bd17ff3fa471d097e4ac7dd99eec1125f2cd6fd2
MoKaEins <150668160+MoKaEins@users.noreply.github.com>
2023-12-10 11:37:21 +0100

    brought back umlaut layer

--- keyboards/lily58/keymaps/MoKaEins/keymap.c
+++ keyboards/lily58/keymaps/MoKaEins/keymap.c
@@ -6,26 +6,27 @@ enum layer_number {
   _NUM,
   _ARROW,
   _GAMING,
+  _UMLAUT
 };
 
 
 // Tap Dance declarations
 enum {
-    TD_E_ACC,
-    TD_A_AE,
-    TD_O_OE,
-    TD_U_UE,
-    TD_S_SS,
+    // TD_E_ACC,
+    // TD_A_AE,
+    // TD_O_OE,
+    // TD_U_UE,
+    // TD_S_SS,
     TD_RSFT_ENT
 };
 
 // Tap Dance definitions
 tap_dance_action_t tap_dance_actions[] = {
-    [TD_E_ACC] = ACTION_TAP_DANCE_DOUBLE(KC_E, KC_EQL),
-    [TD_O_OE] = ACTION_TAP_DANCE_DOUBLE(KC_O, KC_SCLN),
-    [TD_U_UE] = ACTION_TAP_DANCE_DOUBLE(KC_U, KC_LBRC),
-    [TD_S_SS] = ACTION_TAP_DANCE_DOUBLE(KC_S, KC_MINS),
-    [TD_A_AE] = ACTION_TAP_DANCE_DOUBLE(KC_A, KC_QUOT),
+    // [TD_E_ACC] = ACTION_TAP_DANCE_DOUBLE(KC_E, KC_EQL),
+    // [TD_O_OE] = ACTION_TAP_DANCE_DOUBLE(KC_O, KC_SCLN),
+    // [TD_U_UE] = ACTION_TAP_DANCE_DOUBLE(KC_U, KC_LBRC),
+    // [TD_S_SS] = ACTION_TAP_DANCE_DOUBLE(KC_S, KC_MINS),
+    // [TD_A_AE] = ACTION_TAP_DANCE_DOUBLE(KC_A, KC_QUOT),
     [TD_RSFT_ENT] = ACTION_TAP_DANCE_DOUBLE(KC_RSFT, KC_ENT)
 };
 
@@ -33,10 +34,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 [_BASE] =  LAYOUT(
       KC_ESC, KC_1, KC_2, KC_3, KC_4, KC_5,                               KC_6, KC_7, KC_8, KC_9, KC_0, KC_MINS,
-      KC_TAB, KC_Q, KC_W, KC_F, KC_P, KC_G,                               KC_J, KC_L, TD(TD_U_UE), KC_Y, KC_RBRC, KC_BSLS,
-      KC_LSFT, TD(TD_A_AE), KC_R, TD(TD_S_SS), KC_T, KC_D,                KC_H, KC_N, TD(TD_E_ACC), KC_I, TD(TD_O_OE), TD(TD_RSFT_ENT),
-      MO(_GAMING), KC_Z, KC_X, KC_C, KC_V, KC_B, MO(_NUM),                MO(_FKEYS), KC_K, KC_M, KC_COMM, KC_DOT, KC_SLSH, TT(_GAMING),
-      KC_LGUI, KC_LALT, KC_LCTL, LT(_FKEYS, KC_SPC),                      KC_SPC, KC_BSPC, KC_DEL, MO(_ARROW)
+      KC_TAB, KC_Q, KC_W, KC_F, KC_P, KC_G,                               KC_J, KC_L, KC_U, KC_Y, KC_RBRC, KC_BSLS,
+      KC_LSFT, KC_A, KC_R, KC_S, KC_T, KC_D,                              KC_H, KC_N, KC_E, KC_I, KC_O, TD(TD_RSFT_ENT),
+      MO(_GAMING), KC_Z, KC_X, KC_C, KC_V, KC_B, MO(_NUM),    MO(_FKEYS), KC_K, KC_M, KC_COMM, KC_DOT, KC_SLSH, MO(_UMLAUT),
+      KC_LGUI, KC_LALT, KC_LCTL, LT(_UMLAUT, KC_SPC),                     KC_SPC, KC_BSPC, KC_DEL, MO(_ARROW)
 ),
 [_FKEYS] = LAYOUT(
       KC_GRV, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5,                     KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11,
@@ -62,9 +63,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 [_GAMING] = LAYOUT(
       KC_ESC, KC_1, KC_2, KC_3, KC_4, KC_5,               KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-      KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T,               KC_Z, KC_U, KC_I, KC_O, KC_VOLU, KC_TRNS,
-      KC_LSFT, KC_A, KC_S, KC_D, KC_F, KC_G,               KC_H, KC_J, KC_K, RALT(KC_NUBS), KC_SCLN, TO(_BASE),
-      KC_LCTL, KC_Y, KC_X, KC_C, KC_V, KC_B, KC_NO,      KC_N, KC_TRNS, KC_TRNS, KC_NUBS, LSFT(KC_NUBS), KC_VOLD, KC_TRNS,
+      KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T,               KC_Z, KC_U, KC_I, KC_O, KC_P, KC_BSLS,
+      KC_LSFT, KC_A, KC_S, KC_D, KC_F, KC_G,               KC_H, KC_J, KC_K, KC_L, KC_SCLN, TO(_BASE),
+      KC_LCTL, KC_Y, KC_X, KC_C, KC_V, KC_B, KC_NO,      KC_N, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
       KC_RCTL, KC_LGUI, KC_LALT, KC_SPC,                  KC_NO, KC_DEL, KC_NO, KC_NO
+  ),
+
+[_UMLAUT] = LAYOUT(
+      KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+      KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                        KC_TRNS, KC_TRNS, KC_LBRC, KC_TRNS, KC_VOLU, KC_TRNS,
+      KC_TRNS, KC_QUOT, KC_TRNS, KC_MINS, KC_TRNS, KC_TRNS,                        KC_TRNS, KC_TRNS, KC_EQL, RALT(KC_NUBS), KC_SCLN, KC_TRNS,
+      KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,      KC_TRNS, KC_TRNS, KC_TRNS, KC_NUBS, LSFT(KC_NUBS), KC_VOLD, KC_TRNS,
+      KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                                          KC_ENT, KC_TRNS, KC_TRNS, KC_TRNS
   )
 };
