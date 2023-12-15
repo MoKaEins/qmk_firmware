121fe296fb013afbe6068e0957d581a02b45292e
MoKaEins <150668160+MoKaEins@users.noreply.github.com>
2023-12-15 15:11:58 +0100

    cursor keys

--- keyboards/lily58/keymaps/MoKaEins/keymap.c
+++ keyboards/lily58/keymaps/MoKaEins/keymap.c
@@ -35,24 +35,24 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       KC_ESC, KC_1, KC_2, KC_3, KC_4, KC_5,                               KC_6, KC_7, KC_8, KC_9, KC_0, KC_MINS,
       KC_TAB, KC_Q, KC_W, KC_F, KC_P, KC_G,                               KC_J, KC_L, KC_U, KC_Y, KC_RBRC, KC_BSLS,
       KC_LSFT, KC_A, KC_R, KC_S, KC_T, KC_D,                              KC_H, KC_N, KC_E, KC_I, KC_O, TD(TD_RSFT_ENT),
-      TO(_GAMING), KC_Z, KC_X, KC_C, KC_V, KC_B, MO(_NUM),    MO(_NUM), KC_K, KC_M, KC_COMM, KC_DOT, KC_SLSH, MO(_UMLAUT),
-      KC_LGUI, KC_LALT, KC_LCTL, LT(_UMLAUT, KC_SPC),                     KC_SPC, KC_BSPC, KC_DEL, MO(_ARROW)
+      KC_LGUI, KC_Z, KC_X, KC_C, KC_V, KC_B, MO(_NUM),          MO(_NUM), KC_K, KC_M, KC_COMM, KC_DOT, KC_SLSH, MO(_ARROW),
+      TT(_ARROW), KC_LALT, KC_LCTL, LT(_UMLAUT, KC_SPC),                  KC_SPC, KC_BSPC, KC_LEFT, KC_RGHT
 ),
 
 [_NUM] = LAYOUT(
       KC_GRV, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5,                                 KC_NUM, KC_P7, KC_P8, KC_P9, KC_PSLS, KC_PAST,
       KC_TRNS, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10,                               KC_NO, KC_P4, KC_P5, KC_P6, KC_PPLS, KC_NO,
-      KC_CAPS, KC_F11, KC_F12, KC_TRNS, KC_TRNS, KC_TRNS,                        KC_NO, KC_P1, KC_P2, KC_P3, KC_PEQL, KC_HOME,
-      KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,      KC_TRNS, KC_NO, KC_P0, KC_PDOT, KC_PCMM, KC_PMNS, KC_END,
-      KC_RGUI, KC_RALT, KC_RCTL, KC_TRNS,                                     KC_TRNS, KC_DEL, KC_TRNS, KC_TRNS
+      KC_CAPS, KC_F11, KC_F12, SGUI(KC_S), KC_PSCR, KC_TRNS,                        KC_NO, KC_P1, KC_P2, KC_P3, KC_PEQL, KC_HOME,
+      KC_RGUI, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,      KC_TRNS, KC_NO, KC_P0, KC_PDOT, KC_PCMM, KC_PMNS, KC_END,
+      KC_TRNS, KC_RALT, KC_RCTL, KC_TRNS,                                     KC_TRNS, KC_DEL, KC_TRNS, KC_TRNS
   ),
 
 [_ARROW] = LAYOUT(
       LCG_SWP, LCG_NRM, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                 KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-      KC_TRNS, RALT(KC_Q), KC_TRNS, KC_UP, KC_TRNS, KC_TRNS,                KC_TRNS, RALT(KC_L), KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-      KC_TRNS, KC_QUOT, KC_LEFT, KC_DOWN, KC_RGHT, KC_NO,                   KC_NO, KC_NO, KC_NO, KC_NO, KC_UP, KC_NO,
-      KC_TRNS, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_TRNS,           KC_NO, KC_NO, KC_NO, KC_NO, KC_LEFT, KC_DOWN, KC_RGHT,
-                          KC_NO, KC_RALT, KC_RCTL, KC_TRNS,              KC_NO, KC_DEL, KC_NO, KC_TRNS
+      KC_TRNS, RALT(KC_Q), KC_TRNS, KC_UP, KC_TRNS, KC_TRNS,                KC_TRNS, RALT(KC_L), KC_UP, KC_TRNS, KC_TRNS, KC_TRNS,
+      KC_TRNS, KC_QUOT, KC_LEFT, KC_DOWN, KC_RGHT, KC_NO,                   KC_NO, KC_LEFT, KC_DOWN, KC_RGHT, KC_TRNS, TO(_GAMING),
+      KC_TRNS, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_TRNS,           KC_NO, KC_NO, KC_NO, KC_NO, KC_TRNS, KC_TRNS, KC_TRNS,
+                          TO(_BASE), KC_RALT, KC_RCTL, KC_TRNS,              KC_NO, KC_DEL, KC_NO, TO(_BASE)
   ),
 
 [_GAMING] = LAYOUT(
@@ -68,6 +68,6 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                        KC_TRNS, KC_TRNS, KC_LBRC, KC_TRNS, KC_VOLU, KC_BRIU,
       KC_TRNS, KC_QUOT, KC_TRNS, KC_MINS, KC_TRNS, KC_TRNS,                        KC_TRNS, KC_TRNS, KC_EQL, RALT(KC_NUBS), KC_SCLN, KC_TRNS,
       KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,      KC_TRNS, KC_TRNS, KC_TRNS, KC_NUBS, LSFT(KC_NUBS), KC_VOLD, KC_BRID,
-      KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                                          KC_ENT, KC_TRNS, KC_TRNS, KC_TRNS
+      KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                                          KC_ENT, KC_DEL, KC_TRNS, KC_TRNS
   )
 };
